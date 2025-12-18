9e39296e5a5e5b4b55f9a972873c04e87420f732
Nakshatra_Singh <nishushalu542@gmail.com>
2025-12-18 19:38:52 +0530

    Components Used(Nav and Card)

---
+++ 02_Components/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='Card'>
+        <h1>Card Used.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, itaque.</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02_Components/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <button>Home</button>
+        <button>View</button>
+        <button>About</button>        
+    </div>
+  )
+}
+
+export default Navbar
