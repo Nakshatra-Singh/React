3fde0b6a2045724fcd7da81d752184b0cb2ab511
Nakshatra_Singh <nishushalu542@gmail.com>
2025-12-22 21:05:55 +0530

    06_Tailwind and 05_Css added.

---
+++ 05_Css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button/button'
+import Header from './Header/header'
+
+const App = () => {
+  return (
+    <div>      
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ 05_Css/src/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.hd}>
+        <h2>DeadShot#BINGO</h2>
+        <button>Visit Tracker</button>
+    </div>
+  )
+}
+
+export default Header
